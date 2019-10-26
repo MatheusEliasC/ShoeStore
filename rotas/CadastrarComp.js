2857093144a289843cec5a3cbb26a72d01b76bc8
Matheus Elias Cruz <matheus.elias.cruz@hotmail.com>
2019-10-26 12:14:25 -0300

    Implemented "Cadastrar" system

--- rotas/CadastrarComp.js
+++ rotas/CadastrarComp.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import {    View,    Text, StyleSheet, Button, TextInput, ScrollView, AsyncStorage} from 'react-native';
-import ImagePicker from 'react-native-image-picker'
+import {    View,    Text, StyleSheet, Button, TextInput, ScrollView, AsyncStorage, Alert, TouchableOpacity} from 'react-native';
+// import ImagePicker from 'react-native-image-picker'
 
 class CadastrarComp extends Component {
     constructor(props){
         super(props);
-        this.state = { modelo: "null" , numero: "null" , sexo: "null" , cor: "null" ,
-                       preco: "null", imagem: "null" , qr: "null", data: Date()};
-        this.handleChoosePhoto = this.handleChoosePhoto.bind(this);
+        this.state = { modelo: "null" , numero: "null" , sexo: "null" , cor: "null" , preco: "null", data: Date()};
+            // imagem: null , qr: "null", };
+        // this.handleChoosePhoto = this.handleChoosePhoto.bind(this);
         this.setTenis = this.setTenis.bind(this);
         this.setSocial = this.setSocial.bind(this);
         this.setCasual = this.setCasual.bind(this);
@@ -27,16 +27,16 @@ class CadastrarComp extends Component {
         title: 'Cadastrar Produto',
     };
 
-    handleChoosePhoto = () => {
-        const options = {
-            noData: true,
-        };
-        ImagePicker.launchImageLibrary(options, response => {
-            if (response.uri) {
-                this.setState({ photo: response })
-            }
-        })
-    };
+    // handleChoosePhoto = () => {
+    //     const options = {
+    //         noData: true,
+    //     };
+    //     ImagePicker.launchImageLibrary(options, response => {
+    //         if (response.uri) {
+    //             this.setState({ image: response })
+    //         }
+    //     })
+    // };
 
     setTenis = () => {
         this.setState({
@@ -71,49 +71,29 @@ class CadastrarComp extends Component {
     };
 
     _storeData = async () => {
-        try {
-            await AsyncStorage.setItem('Calcado_modelo', this.modelo);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_numero', this.numero);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_sexo', this.sexo);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_cor', this.cor);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_preco', this.preco);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_imagem', this.imagem);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_qr', this.qr);
-        } catch (error) {
-            // Error saving data
-        }
-        try {
-            await AsyncStorage.setItem('Calcado_data', this.data);
-        } catch (error) {
-            // Error saving data
-        }
+        const showAlert = () =>{
+            Alert.alert(
+                'Produto Adicionado!'
+            )
+        };
+
+        // if(this.modelo.localeCompare("null")){
+        //
+        // }
+
+        await AsyncStorage.setItem('Calcado_modelo', this.modelo);
+        await AsyncStorage.setItem('Calcado_numero', this.numero);
+        await AsyncStorage.setItem('Calcado_sexo', this.sexo);
+        await AsyncStorage.setItem('Calcado_cor', this.cor);
+        await AsyncStorage.setItem('Calcado_preco', this.preco);
+        await AsyncStorage.setItem('Calcado_data', this.data);
+
+        showAlert();
+
     };
 
     render() {
+        // const { imagem } = this.state;
         return (
             <ScrollView>
             <View>
@@ -139,15 +119,18 @@ class CadastrarComp extends Component {
                 <Text style={styles.texto}>Digite o preco do calçado:</Text>
                 <TextInput onChangeText={preco => this.setState({preco})}/>
 
-                <Text style={styles.texto}>Insira a imagem do calçado:</Text>
-                <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
-                {/*TODO Procurar como fazer um botão de import pra importar a imagem do celular*/}
+                {/*<Text style={styles.texto}>Insira a imagem do calçado:</Text>*/}
+                {/*<Button title="Choose Photo" onPress={this.handleChoosePhoto} />*/}
+                {/*{imagem && ( <Image source={{ uri: imagem.uri }} style={{ width: 300, height: 300 }}/>)}*/}
+                {/*TODO Procurar como fazer um botão de import pra importar a imagem do celular*!/*/}
 
-                <Text style={styles.texto}>Insira o QR da caixa do calçado:</Text>
-                {/*TODO fazer outro import aqui*/}
+                {/*<Text style={styles.texto}>Insira o QR da caixa do calçado:</Text>*/}
+                {/*TODO fazer outro import aqui*!*/}
 
                 <Text style={styles.texto}>A data de entrada do calçado no estoque será do dia de hoje.</Text>
-                <Button title={'Enviar'} onPress={this.setUnissex}/>
+
+                <TouchableOpacity onPress = {this._storeData} style = {styles.enviarButton}><Text>Enviar</Text></TouchableOpacity>
+
             </View>
             </ScrollView>
         );
@@ -170,6 +153,14 @@ const styles = StyleSheet.create({
         margin:1,
         padding:2,
     },
+    enviarButton: {
+        textAlign: 'center',
+        margin: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 7,
+        borderRadius: 5,
+        backgroundColor: "#AEDEF4",
+    }
 });
 
 export default CadastrarComp;
